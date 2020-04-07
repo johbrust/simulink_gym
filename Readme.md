9c81bedab0afbae723155c33bb46a1e3f5344aec
johannes <johannes.brust@uni-osnabrueck.de>
2020-04-07 16:47:05 +0200

    Add core functionality of env class

--- Readme.md
+++ Readme.md
@@ -1,3 +1,35 @@
 # Wrapper for using Simulink models as Gym Environments
 
-This wrapper establishes the gym environment interface for Simulink models.
+---
+
+## To Do
+
+- How to define `actions` and `observations`?
+- How to implement the `render` method?
+- Do the TCP ports for sending and receiving need separate threads?
+- How to initialize the Simulink model?
+
+---
+
+This wrapper establishes the gym environment interface for Simulink models by deriving a `gym_env_simulink_wrapper.Environment` subclass from [`gym.Env`](https://github.com/openai/gym/blob/master/gym/core.py#L8) as a base class.
+
+The wrapper implements the `step`, `reset`, `render`, `close` and `seed` methods of the interface.
+
+## Preparing the Simulink Model
+
+In order to use a Simulink model with this wrapper the model has to be prepared.
+
+__To Do__:
+
+- Sample time?
+- Solver?
+
+### TCP/IP Communication
+
+The wrapper communicates with the model via TCP/IP. The _Instrument Control Toolbox_ provides the [`TCP/IP Send`](https://www.mathworks.com/help/instrument/tcpipsend.html) and [`TCP/IP Receive`](https://www.mathworks.com/help/instrument/tcpipreceive.html) blocks for this communication. By specifying the _Priority_ of the blocks, the execution order of the blocks can be controlled. It is necessary that the _TCP/IP Receive_ block is executed before the _TCP/IP Send_ block, because the model receives the action first and then simulates the model and sends the observations back to the wrapper.
+
+__To Do__:
+
+- Block sample time?
+- Block parameters?
+- Byte order: Intel x86 is Little Endian $\rightarrow$ the `TCP/IP Send` block is configured for Little Endian, the `TCP/IP Receive` block receives integer values and, therefore, does not need byte order configuration
--- environment.py
+++ environment.py
@@ -1,39 +1,118 @@
 import socket
 import logging
+import gym
+import matlab.engine
+import threading
+import struct
+import array
 
 logging.basicConfig(level=logging.DEBUG,
                     format='%(asctime)s (%(name)s, %(levelname)s): %(message)s',
                     datefmt='%d-%b-%y %H:%M:%S')
 
 
-class Environment:
+class Environment(gym.Env):
 
-    SEND_PORT = '42312'
-    RECV_PORT = '42313'
+    SEND_PORT = 42313  # Port on which data is sent to the model
+    RECV_PORT = 42312  # Port on which data is received from the model
 
-    def __init__(self):
-        self.sendSocket = CommSocket(self.SEND_PORT)
-        self.recvSocket = CommSocket(self.RECV_PORT)
+    def __init__(self, absolute_path, env_name):
+        self.recv_socket = CommSocket(self.RECV_PORT)
+        self.recv_socket_thread = threading.Thread(name='recv_socket.connect()', target=self.recv_socket.connect)
+        self.recv_socket_thread.start()
+        self.send_socket = CommSocket(self.SEND_PORT)
+        self.send_socket_thread = threading.Thread(name='send_socket.connect()', target=self.send_socket.connect)
+        self.send_socket_thread.start()
+        # logging.info('Starting Matlab engine')
+        # self.matlab_engine = matlab.engine.start_matlab()
+        # logging.info('Adding path to Matlab path: %s' % absolute_path)
+        # self.matlab_path = self.matlab_engine.addpath(absolute_path)
+        # logging.info('Creating simulation input object for model %s' % env_name)
+        # self.sim_info = self.matlab_engine.Simulink.SimulationInput(env_name)
+        # logging.info('Creating simulation thread')
+        # self.simulation_thread = threading.Thread(name='sim thread', target=self.matlab_engine.sim,
+        #                                           args=(self.sim_info,), daemon=True)
+        # self.simulation_started = False
+        # TODO: check input value validity
+
+    def __del__(self):
+        logging.debug('Deleting Environment')
+        # Wait for connection to be established:
+        self.send_socket_thread.join()
+        self.recv_socket_thread.join()
+
+        # Close sockets:
+        self.send_socket.close()
+        self.recv_socket.close()
+        logging.debug('Deleted Environment')
+
+    def initialize_model(self, params):
+        # TODO: initialize model using params:
+        # self.sim_info = self.matlabEngine.setBlockParameter(self.sim_info, params.block, params.block.param,
+        #                                                    str(params.block.value))
+        pass
 
     def step(self, action):
         # TODO: implement step()
+        # if not self.simulation_started:
+        #     logging.info('Starting simulation thread')
+        #     self.simulation_thread.start()
+        #     self.simulation_started = True
+
+        # Wait for connection to be established:
+        if self.send_socket_thread.is_alive() or self.recv_socket_thread.is_alive():
+            logging.info('Waiting for connection')
+            self.send_socket_thread.join()
+            self.recv_socket_thread.join()
+            logging.info('Connection established')
+
+        # Pass action to send socket:
+        self.send_socket.send(action)
+
+        done = False
+
+        # TODO: Wait until data was received by receive socket
+        recv_data = self.recv_socket.receive()
+        # When the simulation is done an empty message is sent:
+        if not recv_data:
+            observation = None
+            reward = 0
+            done = True
+        else:
+            observation = recv_data[0:-1]  # observations are everything except the last entry
+            reward = recv_data[-1]  # reward is last entry
+
+        info = None
+
+        return observation, reward, done, info
 
     def reset(self):
         # TODO: implement reset()
+        # self.simulation_started = False
+        pass
+
+    def render(self, mode='human'):
+        # TODO: implement render()
+        pass
 
 
 class CommSocket:
 
+    HOST = 'localhost'
+
     def __init__(self, port):
         logging.info('Setting up server on port %d' % port)
-        self.host = 'localhost'
         self.port = port
         self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.server.setblocking(False)
-        self.server.bind(('localhost', self.port))
+        self.server.bind((self.HOST, self.port))
+        self.connection = None
+        self.address = None
+
+    def connect(self, timeout=60):
         self.server.listen(1)
-        logging.info('Listening on port %d' % port)
-        self.server.settimeout(60)
+        logging.info('Listening on port %d' % self.port)
+        self.server.settimeout(timeout)
         try:
             self.connection, self.address = self.server.accept()
             logging.info('Connection established with %s' % self.connection)
@@ -46,10 +125,15 @@ class CommSocket:
             self.server.close()
 
     def receive(self):
-        return self.connection.recv(1024)
+        data = self.connection.recv(2048)
+        logging.debug('Received data: %s' % str(data))
+        data_array = array.array('d', data)
+        return data_array
 
     def send(self, data):
-        self.connection.sendall(data)
+        msg = struct.pack('<B', data)
+        logging.debug('Sending message: ' + str(msg))
+        self.connection.sendall(msg)
 
     def close(self):
         logging.info('Closing connection %s and port %d' % (self.connection, self.port))
