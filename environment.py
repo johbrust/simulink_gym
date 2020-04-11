71898a2bc71dc6bebe0e7f80d6cc59a27927f67d
johannes <johannes.brust@uni-osnabrueck.de>
2020-04-11 13:07:09 +0200

    Add initial values and model debug

--- environment.py
+++ environment.py
@@ -16,24 +16,33 @@ class Environment(gym.Env):
     SEND_PORT = 42313  # Port on which data is sent to the model
     RECV_PORT = 42312  # Port on which data is received from the model
 
-    def __init__(self, absolute_path, env_name):
+    def __init__(self, absolute_path, env_name, initial_values, model_debug=False):
+        # TODO: check input value validity
+        self.simulation_thread = threading.Thread()
+        self.simulation_started = False
+        self.simulation_initialized = False
+        self.initial_obs = initial_values
+        self.initial_step = True
+        self.model_debug = model_debug
+
+        self.env_name = env_name
+
+        # Setup connection via TCP/IP:
         self.recv_socket = CommSocket(self.RECV_PORT)
         self.recv_socket_thread = threading.Thread(name='recv_socket.connect()', target=self.recv_socket.connect)
         self.recv_socket_thread.start()
         self.send_socket = CommSocket(self.SEND_PORT)
         self.send_socket_thread = threading.Thread(name='send_socket.connect()', target=self.send_socket.connect)
         self.send_socket_thread.start()
-        # logging.info('Starting Matlab engine')
-        # self.matlab_engine = matlab.engine.start_matlab()
-        # logging.info('Adding path to Matlab path: %s' % absolute_path)
-        # self.matlab_path = self.matlab_engine.addpath(absolute_path)
-        # logging.info('Creating simulation input object for model %s' % env_name)
-        # self.sim_info = self.matlab_engine.Simulink.SimulationInput(env_name)
-        # logging.info('Creating simulation thread')
-        # self.simulation_thread = threading.Thread(name='sim thread', target=self.matlab_engine.sim,
-        #                                           args=(self.sim_info,), daemon=True)
-        # self.simulation_started = False
-        # TODO: check input value validity
+
+        # Setup Matlab engine:
+        if not self.model_debug:
+            logging.info('Starting Matlab engine')
+            self.matlab_engine = matlab.engine.start_matlab()
+            logging.info('Adding path to Matlab path: %s' % absolute_path)
+            self.matlab_path = self.matlab_engine.addpath(absolute_path)
+            logging.info('Creating simulation input object for model %s' % self.env_name)
+            self.sim_input = self.matlab_engine.Simulink.SimulationInput(self.env_name)
 
     def __del__(self):
         logging.debug('Deleting Environment')
@@ -46,54 +55,108 @@ class Environment(gym.Env):
         self.recv_socket.close()
         logging.debug('Deleted Environment')
 
-    def initialize_model(self, params):
-        # TODO: initialize model using params:
-        # self.sim_info = self.matlabEngine.setBlockParameter(self.sim_info, params.block, params.block.param,
-        #                                                    str(params.block.value))
-        pass
+    def initialize_simulation(self, params):
+        if not self.model_debug:
+            for block in params:
+                # Set simulation input object:
+                logging.info('Setting parameter %s of block %s to value %s' % (block.param, block.path, str(block.value)))
+                self.sim_input = self.matlab_engine.setBlockParameter(self.sim_input, block.path, block.param,
+                                                                      str(block.value))
+
+            logging.info('Creating simulation thread')
+            self.simulation_thread = threading.Thread(name='sim thread', target=self.matlab_engine.sim,
+                                                      args=(self.sim_input,), daemon=True)
+            self.simulation_started = False
+
+        self.simulation_initialized = True
 
     def step(self, action):
-        # TODO: implement step()
-        # if not self.simulation_started:
-        #     logging.info('Starting simulation thread')
-        #     self.simulation_thread.start()
-        #     self.simulation_started = True
+        """
+        TODO
 
-        # Wait for connection to be established:
-        if self.send_socket_thread.is_alive() or self.recv_socket_thread.is_alive():
-            logging.info('Waiting for connection')
-            self.send_socket_thread.join()
-            self.recv_socket_thread.join()
-            logging.info('Connection established')
+        :param action:
+        :return: observation:
+                 reward:
+                 done:
+                 info: dictionary containing simulation timestamp
+        """
 
-        # Pass action to send socket:
-        self.send_socket.send(action)
-
-        done = False
-
-        # TODO: Wait until data was received by receive socket
-        recv_data = self.recv_socket.receive()
-        # When the simulation is done an empty message is sent:
-        if not recv_data:
-            observation = None
-            reward = 0
-            done = True
-        else:
-            observation = recv_data[0:-1]  # observations are everything except the last entry
-            reward = recv_data[-1]  # reward is last entry
-
+        observation = None
+        reward = None
+        done = True
         info = None
+
+        if self.simulation_initialized:
+            if not self.model_debug:
+                if not self.simulation_started:
+                    logging.info('Starting simulation thread')
+                    self.simulation_thread.start()
+                    self.simulation_started = True
+
+            # Wait for connection to be established:
+            if self.send_socket_thread.is_alive() or self.recv_socket_thread.is_alive():
+                logging.info('Waiting for connection')
+                self.send_socket_thread.join()
+                self.recv_socket_thread.join()
+                logging.info('Connection established')
+
+            if not self.initial_step:
+                # Pass action to send socket:
+                self.send_socket.send(action)
+            else:
+                self.initial_step = False
+                logging.debug('Initial step, no action sent')
+
+            done = False
+
+            # TODO: Wait until data was received by receive socket
+            recv_data = self.recv_socket.receive()
+            logging.debug('Received data: %s' % recv_data)
+            # When the simulation is done an empty message is sent:
+            if not recv_data:
+                observation = None
+                reward = 0
+                done = True
+            else:
+                observation = recv_data[0:-2]  # observations are everything except the last entry
+                reward = recv_data[-2]  # reward is last entry
+                sim_time = recv_data[-1]  # simulation timestamp
+
+                info = {'simulation timestamp': str(sim_time)}
+        else:
+            # TODO: Raise error
+            pass
 
         return observation, reward, done, info
 
     def reset(self):
-        # TODO: implement reset()
-        # self.simulation_started = False
-        pass
+        if self.simulation_thread.is_alive():
+            logging.debug('Waiting for simulation to finish')
+            self.simulation_thread.join()
+
+        # Not working (yet), because Simulink closes the connection unilaterally after the simulation finished!
+        # self.recv_socket.close()
+        # self.recv_socket_thread = threading.Thread(name='recv_socket.connect()', target=self.recv_socket.connect)
+        # self.recv_socket_thread.start()
+        # self.send_socket.close()
+        # self.send_socket_thread = threading.Thread(name='send_socket.connect()', target=self.send_socket.connect)
+        # self.send_socket_thread.start()
+
+        logging.info('Creating simulation thread')
+        self.simulation_thread = threading.Thread(name='sim thread', target=self.matlab_engine.sim,
+                                                  args=(self.sim_input,), daemon=True)
+        self.simulation_started = False
+
+        return self.initial_obs
 
     def render(self, mode='human'):
         # TODO: implement render()
         pass
+
+    def set_block_param(self, sim_input, block):
+        # TODO: Necessary?
+        new_sim_input = self.matlab_engine.setBlockParameter(sim_input, block.path, block.param, str(block.value))
+        return new_sim_input
 
 
 class CommSocket:
@@ -126,7 +189,7 @@ class CommSocket:
 
     def receive(self):
         data = self.connection.recv(2048)
-        logging.debug('Received data: %s' % str(data))
+        # logging.debug('Received data: %s' % str(data))
         data_array = array.array('d', data)
         return data_array
 
