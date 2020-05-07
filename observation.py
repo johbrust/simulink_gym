dc3101581c9c302bb3d71e8e44ac9a9a9df78097
johannes <johannes.brust@uni-osnabrueck.de>
2020-05-07 23:27:06 +0200

    Add normalization

--- observation.py
+++ observation.py
@@ -1,40 +1,58 @@
 import numpy as np
-np.set_printoptions(linewidth=100)
+np.set_printoptions(linewidth=100, precision=3)
 
 
 class Observation:
 
-    def __init__(self, name, lower_saturation=-np.inf, upper_saturation=np.inf,
-                 current_value=np.nan, previous_value=np.nan):
+    def __init__(self, name, lower_sat=-np.inf, upper_sat=np.inf,
+                 current_value=np.nan, previous_value=np.nan, normalize=False):
         self.name = name
-        # TODO: Is upper_saturation > lower_saturation?
-        self.upper_saturation = upper_saturation
-        self.lower_saturation = lower_saturation
-        self.current_value = current_value
-        self.previous_value = previous_value
+        if lower_sat >= upper_sat:
+            raise ValueError('Lower saturation value must be less than upper saturation value.')
+        self.upper_saturation = upper_sat
+        self.lower_saturation = lower_sat
+        self.__current_value = current_value
+        self.__previous_value = previous_value
+        if self.lower_saturation > -np.inf and self.upper_saturation < np.inf and normalize:
+            self.normalized = True
+        else:
+            self.normalized = False
+        self.__previous_normalized_value, self.__current_normalized_value = self.normalize_values()
 
     def update_value(self, new_value):
-        self.previous_value = self.current_value
+        self.__previous_value = self.__current_value
         # Limit value:
         if new_value >= self.upper_saturation:
-            self.current_value = self.upper_saturation
+            self.__current_value = self.upper_saturation
         elif new_value <= self.lower_saturation:
-            self.current_value = self.lower_saturation
+            self.__current_value = self.lower_saturation
         else:
-            self.current_value = new_value
+            self.__current_value = new_value
+        # Calculate normalized values:
+        self.__previous_normalized_value, self.__current_normalized_value = self.normalize_values()
+
+    def normalize_values(self):
+        if self.normalized:
+            previous_normalized_value = (self.__previous_value - self.lower_saturation) / \
+                                        (self.upper_saturation - self.lower_saturation)
+            current_normalized_value = (self.__current_value - self.lower_saturation) / \
+                                       (self.upper_saturation - self.lower_saturation)
+            return previous_normalized_value, current_normalized_value
+        else:
+            return np.nan, np.nan
 
     def is_saturated(self):
-        if self.current_value >= self.upper_saturation:
+        if self.__current_value >= self.upper_saturation:
             return True, 1
-        elif self.current_value <= self.lower_saturation:
+        elif self.__current_value <= self.lower_saturation:
             return True, -1
         else:
             return False, 0
 
     def was_saturated(self):
-        if self.previous_value >= self.upper_saturation:
+        if self.__previous_value >= self.upper_saturation:
             return True, 1
-        elif self.previous_value <= self.lower_saturation:
+        elif self.__previous_value <= self.lower_saturation:
             return True, -1
         else:
             return False, 0
@@ -53,6 +71,18 @@ class Observation:
         else:
             return False
 
+    def current_value(self):
+        if self.normalized:
+            return self.__current_normalized_value
+        else:
+            return self.__current_value
+
+    def previous_value(self):
+        if self.normalized:
+            return self.__previous_normalized_value
+        else:
+            return self.__previous_value
+
 
 class Observations:
 
@@ -69,7 +99,7 @@ class Observations:
                 self.observations[i].update_value(np.nan)
 
     def get_current_obs_nparray(self):
-        return np.array([[self.observations[i].current_value for i in range(len(self.observations))]])
+        return np.array([[self.observations[i].current_value() for i in range(len(self.observations))]])
 
     def get_prev_obs_nparray(self):
-        return np.array([[self.observations[i].previous_value for i in range(len(self.observations))]])
+        return np.array([[self.observations[i].previous_value() for i in range(len(self.observations))]])
