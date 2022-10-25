fba32f1f08f36d64201000827d59b054f4206521
Johannes Brust <johannes.brust@gmail.com>
2022-10-26 00:40:33 +0200

    Update parsed arguments

--- examples/train_ppo_cartpole.py
+++ examples/train_ppo_cartpole.py
@@ -27,6 +27,7 @@ def main():
     learning_rate = args.learning_rate
     num_steps = args.num_steps
     num_epochs = args.num_epochs
+    gae_lambda = args.gae_lambda
     
     timestamp = datetime.now().strftime('%Y%m%d.%H%M%S')
     random_tag = ''.join(random.choices(string.ascii_lowercase + string.digits, k=8))
@@ -43,6 +44,7 @@ def main():
               "learning_rate": learning_rate,
               "num_steps": num_steps,
               "num_epochs": num_epochs,
+              "gae_lambda": gae_lambda,
               }
 
     # Weights & Biases (https://wandb.ai):
@@ -75,6 +77,7 @@ def main():
                 learning_rate=config["learning_rate"],
                 n_steps=config["num_steps"],
                 n_epochs=config["num_epochs"],
+                gae_lambda=config["gae_lambda"],
                 verbose=verbose,
                 tensorboard_log=str(log_dir)
                 )
@@ -96,12 +99,14 @@ def define_parser():
     """Define the function interface."""
     parser = argparse.ArgumentParser(description='Training a PPO agent on the Simulink Cartpole environment',
                                      formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('-a', '--gae_lambda', metavar='gae_lambda', type=float, default=0.95,
+                        help='Factor for trade-off of bias vs variance for Generalized Advantage Estimator')
     parser.add_argument('-b', '--batch_size', metavar='batch_size', type=int, default=64,
                         help='Minibatch size for gradient update')
     parser.add_argument('-d', '--log_dir', metavar='log_dir', type=str, default='./logs',
                         help='Path for logs and optional saved policy')
     parser.add_argument('-e', '--num_epochs', metavar='num_epochs', type=int, default=10,
-                        help='Number of epoch when optimizing the surrogate loss')
+                        help='Number of epochs when optimizing the surrogate loss')
     parser.add_argument('-g', '--gamma', metavar='discount_factor', type=float, default=0.99,
                         help='Discount factor of the Bellman update')
     parser.add_argument('-l', '--learning_rate', metavar='learning_rate', type=float, default=3e-4,
@@ -110,7 +115,7 @@ def define_parser():
                         help='The number of steps to run for each environment per update (rollout length)')
     parser.add_argument('-s', '--save_policy', action='store_true',
                         help='Flag for saving the trained policy')
-    parser.add_argument('-t', '--total_timesteps', metavar='total_timesteps', type=int, default=25000,
+    parser.add_argument('-t', '--total_timesteps', metavar='total_timesteps', type=int, default=300000,
                         help='Number of total timesteps to train')
     parser.add_argument('-v', '--verbose', metavar='verbose', type=int, default=0,
                         help='Verbosity level: 0 for no output, 1 for info messages (such as device or wrappers used), 2 for debug messages')
