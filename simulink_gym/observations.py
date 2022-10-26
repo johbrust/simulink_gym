09fd1c8a6cfc9878d74647b83300dd34090e7e5e
Johannes Brust <johannes.brust@gmail.com>
2022-10-26 16:56:42 +0200

    Add docstrings and type hints, reduce debug logging
    Completes #101, #81

--- simulink_gym/observations.py
+++ simulink_gym/observations.py
@@ -5,6 +5,8 @@ import numpy as np
 
 
 class Observation:
+    """Class for representation of environment observations."""
+
     def __init__(
         self,
         name: str,
@@ -12,56 +14,96 @@ class Observation:
         high: float,
         initial_value_path: str,
         initial_value: Union[int, float] = None,
-        reinitialize = False
+        reinitialize: bool = False
     ):
+        """Class representing environment observations.
+        
+        Parameters:
+            name: string
+                name of the observation
+            low: float
+                lower boundary of the observation value, can also be -numpy.inf, used to
+                define the observation space
+            high: float
+                higher boundary of the observation value, can also be numpy.inf, used to
+                define the observation space
+            initial_value_path: string
+                path of the block parameter for setting the initial value (see BlockParam.parameter_path)
+            initial_value: int or float, default: None
+                initial value of the observation (see BlockParam.value), the value will be sampled
+                from the observation space if None
+            reinitialize: bool, default: False
+                flag for reinitialization when resetting the environment
+        """
         self.name = name
         self.space = Box(low=np.array([low], dtype=np.float32), high=np.array([high], dtype=np.float32))
 
+        # Sample initial value if not defined:
         if initial_value is None:
             initial_value = self.space.sample()
         else:
             initial_value = np.array([initial_value], dtype=np.float32)
 
-        if not self.space.contains(initial_value):
-            raise ValueError(
-                f"Observation {self.name}: Initial value {initial_value} not inside space limits"
-            )
+        self._check_initial_value(initial_value)
 
         self.reinitialize = reinitialize
         self.block_param = BlockParam(initial_value_path, initial_value)
 
+    def _check_initial_value(self, value):
+        if not self.space.contains(value):
+            raise ValueError(
+                f"Observation {self.name}: Initial value {value} not inside space limits"
+            )
+
     @property
     def initial_value(self):
+        """Initial value of the observation."""
         return self.block_param.value
 
     @initial_value.setter
     def initial_value(self, value):
+        """Set method for the initial value"""
+        self._check_initial_value(value)
         self.block_param.value = value
 
     def resample_initial_value(self):
+        """Resample the initial value according to observation space."""
         self.block_param.value = self.space.sample()
 
 
 class Observations:
+    """Class representing multiple environment observations as a list."""
+    
     def __init__(self, observations: List[Observation]):
+        """Class representing multiple environment observations as a list.
+        
+        Parameters:
+            observations: list of observations
+        """
         self._observations = observations
+        # Create combined observation space from single observations:
         lows = np.array([observation.space.low[0] for observation in self._observations], dtype=np.float32)
         highs = np.array([observation.space.high[0] for observation in self._observations], dtype=np.float32)
         self.space = Box(low=lows, high=highs)
 
     def __getitem__(self, index: int):
+        """Method for indexing of observations list."""
         return self._observations[index]
 
     def __iter__(self):
+        """Method for iterating over observations list."""
         return self._observations.__iter__()
 
     def __next__(self):
+        """Method for getting next observation in list."""
         return self._observations.__next__()
 
     def __len__(self):
+        """Method for determining number of observations."""
         return len(self._observations)
 
     @property
     def initial_state(self):
+        """Combined initial state of all observations as numpy array."""
         initial_state = [obs.initial_value[0] for obs in self._observations]
         return np.array(initial_state)
