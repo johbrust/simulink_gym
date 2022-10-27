6e08d750002ab1e54c566aa903357224499eb245
Johannes Brust <johannes.brust@gmail.com>
2022-10-28 00:03:29 +0200

    Setup pyproject.toml for package management, update version
    Closes #41, #54, #86

--- simulink_gym/__init__.py
+++ simulink_gym/__init__.py
@@ -1,6 +1,9 @@
-from pathlib import Path
+"""Simulink Gym: Gym Interface Wrapper for Simulink Models"""
+
+__version__ = '0.5.0'
 
 # Path of the simulink block library defining the interface blocks:
+from pathlib import Path
 SIMULINK_BLOCK_LIB_PATH = Path(__file__).parent.parent.absolute().joinpath('simulink_block_lib')
 
 from gym import logger
