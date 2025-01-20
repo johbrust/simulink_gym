bcd84fbc83f3fd2438f1fb0b6b72f32b2c837ae0
Johannes Brust <johannes.brust@gmail.com>
2025-01-20 16:45:19 +0100

    Add logging level setter

--- simulink_gym/__init__.py
+++ simulink_gym/__init__.py
@@ -11,6 +11,7 @@ SIMULINK_BLOCK_LIB_PATH = (
 )
 
 import sys
+from typing import Union
 
 from gymnasium import spaces
 from loguru import logger
@@ -18,12 +19,24 @@ from loguru import logger
 from .environment import SimulinkEnv
 from .observations import Observation, Observations
 
+
+def setLoggerLevel(logger, level: Union[str, int]) -> None:
+    """
+    Set the logging level of a loguru logger.
+
+    Args:
+        logger: loguru logger instance
+        level: string or int representing the desired loguru logger level
+    """
+    logger.remove()
+    logger.add(sys.stderr, level=level)
+
 # Set logging level to INFO:
-logger.remove()
-logger.add(sys.stderr, level="INFO")
+setLoggerLevel(logger, level="INFO")
 
 __all__ = [
     logger,
+    setLoggerLevel,
     spaces,
     SimulinkEnv,
     Observation,
